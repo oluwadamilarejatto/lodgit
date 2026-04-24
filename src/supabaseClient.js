8add70028c60f4633086f71b812afd0f39644511
oluwadamilarejatto <oluwadamilarejatto01@gmail.com>
2026-04-24 07:17:01 +0100

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://tcjgiglbmxiusfnthj.supabase.co'
+const supabaseUrl = 'https://tcjgigltxrxiuslfnthj.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjamdpZ2x0eHJ4aXVzbGZudGhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY4OTYyNjMsImV4cCI6MjA5MjQ3MjI2M30.UxpJ7EFMSrbrqgJhK8_8ajnkE8pswhjbblXcwXZUwc0'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
