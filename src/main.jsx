881125e0054468be85c2cc4f98254892b934992a
oluwadamilarejatto <oluwadamilarejatto01@gmail.com>
2026-04-24 20:33:55 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import LodgitAuth from './LodgitAuth'
 import Lodgit from './Lodgit'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Lodgit />
+    <LodgitAuth>
+      {({ session, workspace, onSignOut }) => (
+        <Lodgit session={session} workspace={workspace} onSignOut={onSignOut} />
+      )}
+    </LodgitAuth>
   </React.StrictMode>
 )
